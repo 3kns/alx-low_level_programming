b97885abc9be1ba91ceea923dad7cb629af81cfd
Jeffery Ocran <ocranjeffery@gmail.com>
2023-02-19 05:02:38 -0800

    nice

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdio.lib>
+/**
+ * main - Entry point with "void" argument
+ *
+ * Description: "the program's description"
+ *
+ * Return - Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	do {
+		putchar(ch);
+		ch++
+	} while (ch <= 'z');
+	break;
+
+	return (0);
+}
