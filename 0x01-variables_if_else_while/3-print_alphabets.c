421d945d0bb22b36ba69c1a5b3d49cd5d24be229
Jeffery Ocran <ocranjeffery@gmail.com>
2023-02-19 08:21:17 -0800

    h

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+ /**
+  * main - Entry point
+  *
+  * Description: "printing alphabets in lower and uoper cases"
+  *
+  * Return - Always 0 (Success)
+  */
+int main(void)
+{
+	char c, C;
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+		if (c == 'z')
+		{
+			break;
+		}
+	}
+	while (C <= 'Z')
+	{
+		putchar(C);
+		C++;
+		if (C == 'Z')
+		{
+			break;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
