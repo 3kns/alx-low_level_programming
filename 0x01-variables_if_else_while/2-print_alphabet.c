98cf016973b970e97f6c419eee99e8476939e2cd
Jeffery Ocran <ocranjeffery@gmail.com>
2023-02-19 06:08:10 -0800

    cool

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Entry point with "void" argument
+ * main - Entry point returns a value of 0
  *
  * Description: "the program's description"
  *
  * Return - Always 0 (Success)
  *
  */
-int main(void)
+void main(void)
 {
 	char ch = 'a';
 
