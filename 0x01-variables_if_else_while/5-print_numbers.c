f512072bd07cc55fdc5d941f91e3c433bd324025
Jeffery Ocran <ocranjeffery@gmail.com>
2023-02-19 11:43:43 -0800

    h

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * main: "Entry point returns a value of 0"
+ *
+ * @void: "always return value 0"
+ *
+ * Description: "the program's description"
+ *
+ * Return: "Always 0 (Success)"
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	return (0);
+}
