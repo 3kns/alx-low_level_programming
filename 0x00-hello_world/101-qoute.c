aea221c3ef7caea2cbdb768beacff8830ae0767a
Jeffery Ocran <ocranjeffery@gmail.com>
2023-02-20 12:45:02 -0800

    h

---
+++ 0x00-hello_world/101-qoute.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * main: "Entry point"
+ *
+ * @void: "always return value 0"
+ *
+ * Description: "the program's description"
+ *
+ * Return: "1 (Success)"
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
