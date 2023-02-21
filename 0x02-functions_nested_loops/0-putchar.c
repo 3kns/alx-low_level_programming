4960c188084001062f827589e84235beca8de37d
Jeffery Ocran <ocranjeffery@gmail.com>
2023-02-21 11:21:47 -0800

    functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * main: "Entry point returns a value of 0"
+ *
+ * @void: "always return value 0"
+ *
+ * Description: "the program's description"
+ *
+ * Return: "Always 0 (Success)"
+ */
+int main(void)
+{
+	writes("\_putchar");
+
+	return (0);
+}
