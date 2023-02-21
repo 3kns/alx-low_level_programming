3670d49df3094e0a4b46b59de6fb9a86d61e73ed
Jeffery Ocran <ocranjeffery@gmail.com>
2023-02-21 14:56:12 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - Entrt point
+ * @void: will return a value of 0
+ * Return: 0 (success)
+ */
+void print_alphabet(void)
+{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
