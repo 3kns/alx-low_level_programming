f8f5bba91b4697aa510faf47f7fe1bf305dc1605
Jeffery Ocran <ocranjeffery@gmail.com>
2023-02-22 14:29:02 -0800

    update

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
  * _abs - entry point
- * @int: type of value to be returned
+ * @n: type of value to be returned
  * Return: absolute value (success)
  */
-int _abs(int)
+int _abs(int n)
 {
-
 	if (n >= 0)
 	{
 		return (n);
