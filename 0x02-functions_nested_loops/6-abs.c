a33bf58a7c49b6b8d9f757f9785c0f503555911e
Jeffery Ocran <ocranjeffery@gmail.com>
2023-02-21 16:42:08 -0800

    abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - entry point
+ * @n: value to be returned
+ * Return: n and -n
+ */
+int _abs(int)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
