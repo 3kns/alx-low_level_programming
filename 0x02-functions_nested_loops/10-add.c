e608d8a7162f450cced8441d95f7a8015b81bc71
Jeffery Ocran <ocranjeffery@gmail.com>
2023-02-22 18:37:02 -0800

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - entry point
+ * @a: value to be added to b
+ * @b: value to be added to a
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
