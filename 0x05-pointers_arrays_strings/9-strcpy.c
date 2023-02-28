e69989b206affc68dd244f70578d1b1e0386ce3d
Jeffery Ocran <ocranjeffery@gmail.com>
2023-02-28 13:47:42 -0800

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,17 +7,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-	int x = 0;
+	int x;
 
-	while (*(src + 1) != '\0')
-	{
-		i++;
-	}
-	for (;x < i; x++)
+	for (x = 0; src[x] != '\0'; x++)
 	{
 		dest[x] = src[x];
 	}
-	dest[i] = '\0';
+	dest[i++] = '\0';
 	return (dest);
 }
