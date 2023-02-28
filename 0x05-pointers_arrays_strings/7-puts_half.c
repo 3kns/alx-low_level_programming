3bec16194da24703016c05116338d9605285cc97
Jeffery Ocran <ocranjeffery@gmail.com>
2023-02-28 04:56:24 -0800

    g

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - entrt point
+ * @str: string in qhestion
+ */
+void puts_half(char *str)
+{
+	int count = 0;
+	int a, n;
+
+	for (a = 0; str[a] != '\0'; a++)
+		count++;
+	
+	n = (count / 2);
+
+	if ((count % 2) == 1)
+		n = ((count + 1) / 2);
+	for (a = n; str[a] != 0; a++)
+		_putchar(str[a]);
+	_putchar('\n');
+}
