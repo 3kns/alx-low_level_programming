c31f2786899738eceb6deb685759de74c6e7d9b1
Jeffery Ocran <ocranjeffery@gmail.com>
2023-02-28 02:08:19 -0800

    six

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts2 - funtion for printing only one character
+ * @str - string involved
+ */
+void puts2(char *str)
+{
+	int count = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		count++;
+	}
+	t = count - 1;
+	for (o = 0; o <= t; o++)
+	{
+		if (o % 2 == 0)
+		{
+			_putchar(str[o]);
+		}
+	}
+	_putchar('\n');
+}
