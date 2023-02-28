9f3712a3add1648f7c3948b51e6087a9f80c627a
Jeffery Ocran <ocranjeffery@gmail.com>
2023-02-28 06:59:42 -0800

    yep

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strcpy - entry point
+ * @dest: declared variable
+ * @src: declared variable
+ * Return: dest variable (success)
+ */
+char *_strcpy(char *dest, char *src);
+{
+	int i = 0;
+	int x = 0;
+
+	while (*(src + 1) != '\0')
+	{
+		i++;
+	}
+	for (x < i; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
